97496a105dd50bdef1e0d9bccd76ac6a0a2413bd
Pedro Marques <junnior.pedro.pm@gmail.com>
2020-07-20 23:33:02 -0300

    add product list on cart

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -25,6 +25,8 @@ function Header({ cartSize }) {
   );
 }
 
-export default connect((state) => ({
+const mapStateToProps = (state) => ({
   cartSize: state.cart.length,
-}))(Header);
+});
+
+export default connect(mapStateToProps)(Header);
